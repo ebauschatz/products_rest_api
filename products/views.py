9b51455e409736f6cff20504161110a9b5175fc1
ebauschatz <eaenglish113@gmail.com>
2022-09-23 11:34:10 -0500

    feat: adding put method to update single product

--- products/views.py
+++ products/views.py
@@ -19,9 +19,15 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk = pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
