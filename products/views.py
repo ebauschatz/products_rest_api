4ff13e7dafa9bc40ad1fc787d6dc2869f2b409eb
ebauschatz <eaenglish113@gmail.com>
2022-09-23 11:12:20 -0500

    feat: adding post endpoint to create new products and updating serializer to return id

--- products/views.py
+++ products/views.py
@@ -6,12 +6,18 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def product_detail(request, pk):
