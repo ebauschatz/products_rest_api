f72ff6d72f3e0a79d40c7289e570c96b0d351188
ebauschatz <eaenglish113@gmail.com>
2022-09-23 10:52:54 -0500

    feat: adding endpoint to return a single product by ID

--- products/views.py
+++ products/views.py
@@ -11,4 +11,11 @@ def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk = pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
